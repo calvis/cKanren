638f6a798338bb5f7ecd6cfb3f637154bb300b00
Claire Alvis <claire.alvis@gmail.com>
2012-01-20 13:33:32 -0500

    Added required and allowed to never-true

--- never-true.scm
+++ never-true.scm
@@ -1,6 +1,6 @@
 (library
   (never-true)
-  (export never-true never-pairo)
+  (export never-true never-pairo required allowed)
   (import (rnrs) (ck))
 
 (define never-true-c
@@ -18,6 +18,41 @@
 (define never-pairo
   (lambda (x)
     (never-true pair? x)))
+
+(define required
+  (lambda (pred? x)
+    (goal-construct (required-c pred? x))))
+
+(define required-c
+  (lambda (pred? x)
+    (lambdam@ (a : s c)
+      (let ((x (walk* x s)))
+        (cond
+          ((pred? x) a)
+          (else ((update-c (build-oc required-c pred? x)) a)))))))
+
+(define required-enforce
+  (lambda (x)
+    (goal-construct (enforce-required))))
+
+(define (enforce-required)
+  (lambdam@ (a : s c)
+    (and (not (find (lambda (oc) (eq? 'required-c (oc->rator oc))) c))
+         a)))
+
+(define allowed
+  (lambda (u v)
+    (goal-construct (allowed-c pred? x))))
+
+(define allowed-c
+  (lambda (pred? x)
+    (lambdam@ (a : s c)
+      (let ((x (walk* x s)))
+        (cond
+          ((pred? x) a)
+          (else ((update-c (build-oc allowed-c pred? x)) a)))))))
+
+(extend-enforce-fns 'required required-enforce)
 
 )
 
