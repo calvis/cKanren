a590ca9dc55f271fd6dd50639833aabb51bea3a0
Claire Alvis <claire.alvis@gmail.com>
2012-03-03 13:37:50 -0500

    Some improvements to copy-before and drop-before, along with some hack-fixes

--- interval-domain.scm
+++ interval-domain.scm
@@ -1,10 +1,12 @@
 (library
   (interval-domain)
   (export range value-dom?
+    ;; for fd
     map-sum null-dom? singleton-dom? singleton-element-dom
     min-dom max-dom memv-dom? intersection-dom diff-dom
-    copy-before drop-before disjoint-dom? make-dom
+    copy-before-dom drop-before-dom disjoint-dom? make-dom
 
+    ;; interval helpers
     interval-difference interval-union interval-intersection
     interval-memq? cons-dom interval-overlap? interval-> interval-<)
   (import (rnrs) (ck) (only (chezscheme) trace-define))
@@ -28,28 +30,19 @@
     (let ((imin (car i)) (imax (cdr i))
           (jmin (car j)) (jmax (cdr j)))
       (cond
-        ;; [1 2][2 3] = [1 3]
-        ;; [1 2][3 4]
         ((or (= imax jmin)
              (= imax (- jmin 1)))
          `((,imin . ,jmax)))
-        ;; [2 3][1 2] = [1 3]
         ((or (= jmax imin)
              (= jmax (- imin 1)))
          `((,jmin . ,imax)))
-        ;; [1 2][4 5] = ([1 2] [4 5])
         ((< imax jmin) `(,i ,j))
-        ;; [4 5][1 2] = ([1 2] [4 5])
         ((< jmax imin) `(,j ,i))
-        ;; [1 5][2 3] = [1 5]
         ((and (<= imin jmin) (>= imax jmax)) `((,imin . ,imax)))
-        ;; [2 3][1 5] = [1 5]
         ((and (<= jmin imin) (>= jmax imax)) `((,jmin . ,jmax)))
-        ;; [1 3][2 5] = [1 5]
         ((and (<= imin jmin) (>= jmax imax)) `((,imin . ,jmax)))
-        ;; [2 5][1 3] = [1 5]
         ((and (<= jmin imin) (>= imax jmax)) `((,jmin . ,imax)))
-        (else (error 'interval-union "Not written yet" i j))))))
+        (else (error 'interval-union "Not defined" i j))))))
 
 
 (define interval-difference
@@ -57,46 +50,30 @@
     (let ((imin (car i)) (imax (cdr i))
           (jmin (car j)) (jmax (cdr j)))
       (cond
-        ;; [1 2] - [3 4] = ([1 2])
         ((> jmin imax) `((,imin . ,imax)))
-        ;; [2 3] - [1 5] = ()
         ((and (<= jmin imin) (>= jmax imax)) `())
-
-        ;; At this point we know j nether misses i nor does it
-        ;; completely envelop i.  There must be some overlap.
-
-        ;; [1 5] - [2 3] = ([1 1] [4 5])
         ((and (< imin jmin) (> imax jmax))
          `((,imin . ,(- jmin 1)) (,(+ jmax 1) . ,imax)))
-        ;; [1 3] - [2 4] = [1 1]
         ((and (< imin jmin) (<= jmin imax))
          `((,imin . ,(- jmin 1))))
-        ;; [1 3] - [0 1] = [2 3]
         ((and (> imax jmax) (<= jmin imin))
          `((,(+ jmax 1) . ,imax)))
-
-        (else (error 'interval-difference "Not written yet" i j))))))
+        (else (error 'interval-difference "Not defined" i j))))))
 
 (define interval-intersection
   (lambda (i j)
     (let ((imin (car i)) (imax (cdr i))
           (jmin (car j)) (jmax (cdr j)))
       (cond
-        ;; [1 5][6 10] = ()
         ((< imax jmin) `())
-        ;; [6 10][1 5] = ()
         ((< jmax imin) `())
-        ;; [1 5][2 3] = ([2 3])
         ((and (<= imin jmin) (>= imax jmax)) `(,j))
-        ;; [2 3][1 5] = ([2 3])
         ((and (<= jmin imin) (>= jmax imax)) `(,i))
-        ;; [1 3][2 5] = ([2 3])
         ((and (<= imin jmin) (<= imax jmax))
          `((,jmin . ,imax)))
-        ;; [2 5][1 3] = ([2 3])
         ((and (<= jmin imin) (<= jmax imax))
          `((,imin . ,jmax)))
-        (else (error 'interval-intersection "Not written yet" i j))))))
+        (else (error 'interval-intersection "Not defined" i j))))))
 
 (define interval-memq?
   (lambda (x intvl)
@@ -111,7 +88,6 @@
         ((null? n*) `())
         (else (cons-dom (car n*) (loop (cdr n*))))))))
 
-;; lazy hack
 (define interval-overlap?
   (lambda (i j)
     (null? (cdr (interval-union i j)))))
@@ -139,7 +115,7 @@
   (lambda (x dom)
     (let loop ((x (if (interval? x) x `(,x . ,x))) (dom dom))
       (cond
-        ((null? dom) `(,x))
+        ((null-dom? dom) `(,x))
         ((interval-overlap? x (car dom))
          (append-dom (interval-union x (car dom)) (cdr dom)))
         ((interval-> x (car dom))
@@ -149,7 +125,7 @@
 (define append-dom
   (lambda (l s)
     (cond
-      ((null? l) s)
+      ((null-dom? l) s)
       (else (cons-dom (car l) (append-dom (cdr l) s))))))
 
 (define map-sum
@@ -158,7 +134,7 @@
       ((loop
          (lambda (dom)
            (cond
-             ((null? dom)
+             ((null-dom? dom)
               (lambdag@ (a) (mzerog)))
              (else
                (conde
@@ -176,7 +152,7 @@
 
 (define singleton-dom?
   (lambda (dom)
-    (and (null? (cdr dom))
+    (and (null-dom? (cdr dom))
          (singleton-interval? (car dom)))))
 
 (define singleton-element-dom
@@ -190,90 +166,92 @@
 (define max-dom
   (lambda (dom)
     (cond
-      ((null? (cdr dom)) (cdar dom))
+      ((null-dom? (cdr dom)) (cdar dom))
       (else (max-dom (cdr dom))))))
 
 (define memv-dom?
   (lambda (v dom)
     (and (value-dom? v)
-         (exists (lambda (x) (interval-memq? v x)) dom))))
+         (exists (lambda (d) (interval-memq? v d)) dom))))
 
 (define intersection-dom
   (lambda (dom1 dom2)
     (cond
-      ((or (null? dom1) (null? dom2)) '())
-      ;; ((< (car dom1) (car dom2))
-      ;;  (intersection-dom (cdr dom1) dom2))
+      ((or (null-dom? dom1) (null-dom? dom2)) '())
       ((interval-< (car dom1) (car dom2))
        (intersection-dom (cdr dom1) dom2))
-      ;; (else (intersection-dom dom1 (cdr dom2)))
       ((interval-> (car dom1) (car dom2))
        (intersection-dom dom1 (cdr dom2)))
-      ;; ((= (car dom1) (car dom2))
-      ;;  (cons (car dom1)
-      ;;    (intersection-dom (cdr dom1) (cdr dom2))))
       (else
-        (append-dom
-          (interval-intersection (car dom1) (car dom2))
-          (intersection-dom
-            (append-dom
-              (interval-difference (car dom1) (car dom2))
-              (cdr dom1))
-            (append-dom
-              (interval-difference (car dom2) (car dom1))
-              (cdr dom2))))))))
-
-;; ([1 2]) ([3 4])
+        (let ((a1 (interval-difference (car dom1) (car dom2)))
+              (a2 (interval-difference (car dom2) (car dom1))))
+          (append-dom
+            (interval-intersection (car dom1) (car dom2))
+            (intersection-dom
+              (append-dom a1 (cdr dom1))
+              (append-dom a2 (cdr dom2)))))))))
 
 (define diff-dom
   (lambda (dom1 dom2)
     (cond
-      ((or (null? dom1) (null? dom2)) dom1)
-      ;; ((< (car dom1) (car dom2))
-      ;;  (cons (car dom1) (diff-dom (cdr dom1) dom2)))
+      ((or (null-dom? dom1) (null-dom? dom2)) dom1)
       ((interval-< (car dom1) (car dom2))
        (cons (car dom1) (diff-dom (cdr dom1) dom2)))
-      ;; (else (diff-dom dom1 (cdr dom2)))
       ((interval-> (car dom1) (car dom2))
        (diff-dom dom1 (cdr dom2)))
-      ;; ((= (car dom1) (car dom2))
-      ;;  (diff-dom (cdr dom1) (cdr dom2)))
       (else
-       (diff-dom
-         (append-dom
-           (interval-difference (car dom1) (car dom2))
-           (cdr dom1))
-         dom2))
-)))
+        (let ((a1 (interval-difference (car dom1) (car dom2)))
+              (a2 (interval-difference (car dom2) (car dom1))))
+          (diff-dom
+            (append-dom a1 (cdr dom1))
+            (append-dom a2 (cdr dom2))))))))
 
-(define copy-before   
+(define copy-before-interval
+  (lambda (pred intvl)
+    (let ((min (car intvl)) (max (cdr intvl)))
+      (let loop ((i min))
+        (cond
+          ((pred i)
+           (if (= min i) `() `((,min . ,(- i 1)))))
+          ((= i max) `())
+          (else (loop (+ i 1))))))))
+
+(define copy-before-dom
   (lambda (pred dom)
     (cond
       ((null? dom) '())
-      ((pred (car-dom dom)) '())
-      (else (cons-dom
-              (car-dom dom)
-              (copy-before pred (cdr-dom dom)))))))
+      ((let ((intvl (car dom)))
+         (and (pred (cdr intvl)) intvl))
+       => (lambda (intvl) (copy-before-interval pred intvl)))
+      (else (cons (car dom) (copy-before-dom pred (cdr dom)))))))
 
-(define drop-before
+(define drop-before-interval
+  (lambda (pred intvl)
+    (let ((min (car intvl)) (max (cdr intvl)))
+      (let loop ((i min))
+        (cond
+          ((pred i) `((,i . ,max)))
+          ((= i max) `())
+          (else (loop (+ i 1))))))))
+
+(define drop-before-dom
   (lambda (pred dom)
     (cond
       ((null? dom) '())
-      ((pred (car-dom dom)) dom)
-      (else (drop-before pred (cdr-dom dom))))))
+      ((let ((intvl (car dom)))
+         (and (pred (cdr intvl)) intvl))
+       => (lambda (intvl)
+            (append (drop-before-interval pred intvl) (cdr dom))))
+      (else (drop-before-dom pred (cdr-dom dom))))))
 
 (define disjoint-dom?
   (lambda (dom1 dom2)
     (cond
       ((or (null? dom1) (null? dom2)) #t)
-      ;; ((< (car dom1) (car dom2))
-      ;;  (disjoint-dom? (cdr dom1) dom2))
       ((interval-< (car dom1) (car dom2))
        (disjoint-dom? (cdr dom1) dom2))
-      ;; (else (disjoint-dom? dom1 (cdr dom2)))
       ((interval-> (car dom1) (car dom2))
        (disjoint-dom? dom1 (cdr dom2)))
-      ;; ((= (car dom1) (car dom2)) #f)
       (else #f))))
 
 )
@@ -367,6 +345,28 @@
             `((3 . 9))
             `((1 . 2) (4 . 5) (7 . 8)))
           `((4 . 5) (7 . 8))))
+
+;; copy-before
+(assert (equal?
+          (copy-before-dom (lambda (x) (>= x 5)) `((1 . 2) (4 . 6) (8 . 10)))
+          `((1 . 2) (4 . 4))))
+(assert (equal?
+          (copy-before-dom (lambda (x) (<= x 5)) `((1 . 2) (4 . 6) (8 . 10)))
+          `()))
+(assert (equal?
+          (copy-before-dom (lambda (x) (>= x 7)) `((1 . 2) (4 . 6) (8 . 10)))
+          `((1 . 2) (4 . 6))))
+(assert (equal?
+          (copy-before-dom (lambda (x) (>= x 10)) `((1 . 2) (4 . 6) (8 . 10)))
+          `((1 . 2) (4 . 6) (8 . 9))))
+(assert (equal?
+          (copy-before-dom (lambda (x) (>= x 2)) `((1 . 2) (4 . 6) (8 . 10)))
+          `((1 . 1))))
+
+;; drop-before
+(assert (equal?
+          (drop-before-dom (lambda (x) (>= x 5)) `((1 . 2) (4 . 6) (8 . 10)))
+          `((5 . 6) (8 . 10))))
 
 (printf "Assertions passed!\n")
 
