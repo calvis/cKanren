44b932cdfb55ea49fe1c8867e489a684f19479d2
Claire Alvis <claire.alvis@gmail.com>
2013-04-30 12:37:18 -0400

    adding exports to the lang

--- cKanren/main.rkt
+++ cKanren/main.rkt
@@ -1,12 +1,13 @@
 #lang racket/base
 
 (require "ck.rkt"
-         (for-syntax "ck.rkt" racket/syntax))
+         (for-syntax "ck.rkt" racket/syntax racket/base))
 
 (provide 
  (except-out (all-from-out racket/base) #%app)
  conde conda condu fresh succeed fail gen:mk-struct
  run run* prt prtm trace-define use-constraints
- debug
+ debug (for-syntax (all-from-out racket/base) search-strategy)
+ define-lazy-goal
  (rename-out [#%app-safe #%app]))
 
